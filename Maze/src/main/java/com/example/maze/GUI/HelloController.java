3d02153ed8cc42bb4246ae4d3d72ec1a098288f1
budabob1901 <kjemsfrederik@gmail.com>
2026-03-12 15:14:15 +0100

    1

---
+++ Maze/src/main/java/com/example/maze/GUI/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.maze.GUI;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
