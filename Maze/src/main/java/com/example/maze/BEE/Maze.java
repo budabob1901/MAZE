5b5fba8c408b4c3801f2aa92e7471d6fc36aaaf5
budabob1901 <kjemsfrederik@gmail.com>
2026-03-12 15:42:04 +0100

    Maze class

--- Maze/src/main/java/com/example/maze/BEE/Maze.java
+++ Maze/src/main/java/com/example/maze/BEE/Maze.java
@@ -1,4 +1,24 @@
 package com.example.maze.BEE;
 
 public class Maze {
+    private int rows;
+    private int cols;
+    private int[][] map;
+
+    public Maze(int rows, int cols){
+
+        this.rows = rows;
+        this.cols = cols;
+    }
+
+    public void setcell(int rows, int cols, int value){
+        map[rows][cols] = value;
+    }
+
+    public int getCell(int rows, int cols){
+        return map[rows][cols];
+    }
+    public int[][] getMap(){
+        return map;
+    }
 }
